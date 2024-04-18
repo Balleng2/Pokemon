40a0aad3c5f95be7caf78c718dd8b452546e2c7c
Balleng2 <balleng2@aithenticate.za.com>
2024-04-18 14:29:05 +0200

    Initial Commit

---
+++ Pokemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by Balleng Balleng on 2024/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
